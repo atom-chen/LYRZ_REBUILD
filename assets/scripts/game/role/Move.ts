04b25f9f526d7880cc022625e8c7914429e4c304
kongjunjun <969064752@qq.com>
2019-05-22 20:00:13 +0800

    使用面向接口解耦移动组件

--- assets/scripts/game/role/Move.ts
+++ assets/scripts/game/role/Move.ts
@@ -1,9 +1,24 @@
+import { ITouch } from "../../Interface/ITouch";
+
 const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class Move extends cc.Component {
+    private iTouch: ITouch = null;
+    private curDirec: cc.Vec2 = null;
+    private speed: number = 10;
 
     public onLoad(): void {
-        //this.node.on()
+    }
+
+    public update(dt): void {
+        this.computeMove(dt);
+    }
+
+    private computeMove(delTime: number): void {
+        this.curDirec = cc.Vec2.UP// this.iTouch.getDirection();
+        if (this.curDirec && this.curDirec != cc.Vec2.ZERO) {
+            this.node.setPosition(this.node.position.add(this.curDirec.mul(this.speed * delTime)));
+        }
     }
 }
